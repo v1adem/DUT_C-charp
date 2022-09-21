60153f79ff7c89af6be79254cb350e357d973cf5
v1adem <v1adem@ukr.net>
2022-09-21 10:23:47 +0300

    1-2 lab added

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Linq;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1 = 0, t1 = 0, v2 = 0, t2 = 0;
+
+            Console.WriteLine("Input v1 - ");
+            string v1r = Console.ReadLine();
+            double.TryParse(v1r, out v1);
+
+            Console.WriteLine("Input t1 - ");
+            string t1r = Console.ReadLine();
+            double.TryParse(t1r, out t1);
+
+            Console.WriteLine("Input v2 - ");
+            string v2r = Console.ReadLine();
+            double.TryParse(v2r, out v2);
+
+            Console.WriteLine("Input t2 - ");
+            string t2r = Console.ReadLine();
+            double.TryParse(t2r, out t2);
+
+
+            double t = ((v1 * t1) + (v2 * t2)) / (v1 + v2);
+
+            Console.WriteLine($"Temperature of solution - {t}°c");
+        }
+    }
+}
---
+++ Lab2/lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nk = -1;
+            while (nk < 0)
+            {
+                Console.WriteLine("Input NK - ");
+                string strNK = Console.ReadLine();
+                int.TryParse(strNK, out nk);
+                if (nk < 0) Console.WriteLine("Error nk < 0");
+            }
+
+            int nn = -1;
+            while (nn < 0)
+            {
+                Console.WriteLine("Input NN - ");
+                string strNN = Console.ReadLine();
+                int.TryParse(strNN, out nn);
+                if (nn < 0) Console.WriteLine("Error nn < 0");
+                if (nk < nn) Console.WriteLine("Erroe nn > nk");
+            }
+
+            double result = 0;
+            while (nn <= nk)
+            {
+                result += (3 * nn + Math.Pow(-1, (Math.Pow(nn, 2) - nn + 1) * nn)) / (2 * Math.Pow(nn, 2) + 1);
+                nn++;
+            }
+
+            Console.WriteLine($"Result : {result}");
+            
+
+            
+        }
+    }
+}
