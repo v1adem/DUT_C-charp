95bacffafd3a8961e7bc325cdf2ec349ebf801bd
v1adem <v1adem@ukr.net>
2022-10-02 05:32:18 +0300

    3 lab added

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3
+{
+    class Program
+    {
+        static int Main(string[] args) //12 variant
+        {
+            int input = 0;
+            string strInput = "";
+            Console.WriteLine("Input - ");
+            strInput = Console.ReadLine();
+
+            try
+            {
+                int.Parse(strInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error - input is not number");
+                return -1;
+            }
+
+            if (strInput.Length != 3)
+            {
+                Console.WriteLine("Error - input is not three-digit");
+                return -2;
+            }
+
+            if (strInput[0] != strInput[1] && strInput[1] != strInput[2] && strInput[0] != strInput[2])
+            {
+                Console.WriteLine("TRUE - this three-digit number has uniq digits");
+            }
+            else
+            {
+                Console.WriteLine("FALSE - this three-digit number has not uniq digits");
+            }
+            return 0;
+        }
+    }
+}
